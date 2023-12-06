1da2efa86911e738bd3b18211b6fa204764fa2ff
Zach Poba-Nzaou <zachpoba@gmail.com>
2023-12-06 08:36:10 -0500

    added flavor to about page

--- portfolio-website/app/about/page.js
+++ portfolio-website/app/about/page.js
@@ -7,9 +7,29 @@ export default function Home() {
       <main className="flex min-h-screen flex-col bg-[#010303]">
         <Navbar />
         <div className="container mx-auto px-12">
-          <h1 className="text-white">This is the about page</h1>
-          <p className="text-white">test123</p>
-          <p className="text-white">test1234</p>
+          <h1 className="text-white">About</h1>
+          <p className="text-white space-y-6 py-5">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+          <p className="text-white space-y-6 py-5">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
         </div>
       </main>
     </>
